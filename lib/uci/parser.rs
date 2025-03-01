1133f04f332ad5b589a9488c5e7e332b93afd96e
Bruno Dutra <brunocodutra@gmail.com>
2025-03-01 14:20:49 +0100

    relax uci parser even further

--- lib/uci/parser.rs
+++ lib/uci/parser.rs
@@ -35,27 +35,52 @@ where
     preceded(t(tag(key)), value)
 }
 
-type Gather5Output<OA, OB, OC, OD, OE> =
-    (Option<OA>, Option<OB>, Option<OC>, Option<OD>, Option<OE>);
-
-pub fn gather5<'s, OA, OB, OC, OD, OE, A, B, C, D, E>(
-    (mut a, mut b, mut c, mut d, mut e): (A, B, C, D, E),
-) -> impl Parser<&'s str, Output = Gather5Output<OA, OB, OC, OD, OE>, Error = Error<&'s str>>
+#[allow(clippy::type_complexity)]
+pub fn gather<'s, A, B, C, D, E, F, G, H, I, J>(
+    inner: (A, B, C, D, E, F, G, H, I, J),
+) -> impl Parser<
+    &'s str,
+    Output = (
+        Option<<A as Parser<&'s str>>::Output>,
+        Option<<B as Parser<&'s str>>::Output>,
+        Option<<C as Parser<&'s str>>::Output>,
+        Option<<D as Parser<&'s str>>::Output>,
+        Option<<E as Parser<&'s str>>::Output>,
+        Option<<F as Parser<&'s str>>::Output>,
+        Option<<G as Parser<&'s str>>::Output>,
+        Option<<H as Parser<&'s str>>::Output>,
+        Option<<I as Parser<&'s str>>::Output>,
+        Option<<J as Parser<&'s str>>::Output>,
+    ),
+    Error = Error<&'s str>,
+>
 where
-    A: Parser<&'s str, Output = OA, Error = Error<&'s str>>,
-    B: Parser<&'s str, Output = OB, Error = Error<&'s str>>,
-    C: Parser<&'s str, Output = OC, Error = Error<&'s str>>,
-    D: Parser<&'s str, Output = OD, Error = Error<&'s str>>,
-    E: Parser<&'s str, Output = OE, Error = Error<&'s str>>,
+    A: Parser<&'s str, Error = Error<&'s str>>,
+    B: Parser<&'s str, Error = Error<&'s str>>,
+    C: Parser<&'s str, Error = Error<&'s str>>,
+    D: Parser<&'s str, Error = Error<&'s str>>,
+    E: Parser<&'s str, Error = Error<&'s str>>,
+    F: Parser<&'s str, Error = Error<&'s str>>,
+    G: Parser<&'s str, Error = Error<&'s str>>,
+    H: Parser<&'s str, Error = Error<&'s str>>,
+    I: Parser<&'s str, Error = Error<&'s str>>,
+    J: Parser<&'s str, Error = Error<&'s str>>,
 {
+    let (mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h, mut i, mut j) = inner;
+
     move |input: &'s str| {
-        let mut output = (None, None, None, None, None);
+        let mut output = (None, None, None, None, None, None, None, None, None, None);
 
         let a = |s| a.parse(s);
         let b = |s| b.parse(s);
         let c = |s| c.parse(s);
         let d = |s| d.parse(s);
         let e = |s| e.parse(s);
+        let f = |s| f.parse(s);
+        let g = |s| g.parse(s);
+        let h = |s| h.parse(s);
+        let i = |s| i.parse(s);
+        let j = |s| j.parse(s);
 
         let inner = alt((
             a.map(|o| output.0 = Some(o)),
@@ -63,9 +88,40 @@ where
             c.map(|o| output.2 = Some(o)),
             d.map(|o| output.3 = Some(o)),
             e.map(|o| output.4 = Some(o)),
+            f.map(|o| output.5 = Some(o)),
+            g.map(|o| output.6 = Some(o)),
+            h.map(|o| output.7 = Some(o)),
+            i.map(|o| output.8 = Some(o)),
+            j.map(|o| output.9 = Some(o)),
         ));
 
-        let (rest, _) = many1(inner).parse(input)?;
+        let (rest, _) = fold_many0(inner, || (), |_, _| ()).parse(input)?;
         Ok((rest, output))
     }
 }
+
+macro_rules! define_gather {
+    ($gather:ident, $recurse:ident, $($i:ident),+) => {
+        #[allow(non_snake_case, clippy::type_complexity)]
+        pub fn $gather<'s, $($i),+>(inner: ($($i),+)) -> impl Parser<
+            &'s str,
+            Output = ($(Option<<$i as Parser<&'s str>>::Output>),+),
+            Error = Error<&'s str>,
+        >
+        where
+            $($i: Parser<&'s str, Error = Error<&'s str>>),+
+        {
+            let ($($i),+) = inner;
+            $recurse(($($i),+, fail::<_, (), _>())).map(|($($i),+, _)| ($($i),+))
+        }
+    };
+}
+
+define_gather!(gather9, gather, A, B, C, D, E, F, G, H, I);
+define_gather!(gather8, gather9, A, B, C, D, E, F, G, H);
+define_gather!(gather7, gather8, A, B, C, D, E, F, G);
+define_gather!(gather6, gather7, A, B, C, D, E, F);
+define_gather!(gather5, gather6, A, B, C, D, E);
+define_gather!(gather4, gather5, A, B, C, D);
+define_gather!(gather3, gather4, A, B, C);
+define_gather!(gather2, gather3, A, B);

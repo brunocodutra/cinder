aed9594eef4ab031efb351008197fd28370a59b4
Bruno Dutra <brunocodutra@gmail.com>
2025-03-31 06:13:47 -0700

    remove structs Counter, Timer, and Trigger

--- lib/search/control.rs
+++ lib/search/control.rs
@@ -1,53 +1,162 @@
-use crate::util::{Counter, Timer, Trigger};
-use derive_more::with_trait::{Display, Error};
+use crate::{chess::Position, search::Limits};
+use std::ops::Range;
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use std::time::{Duration, Instant};
 
-/// Indicates the search was interrupted .
-#[derive(Debug, Display, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Error)]
-#[display("the search was interrupted")]
-pub struct Interrupted;
-
-/// The search control.
-#[derive(Debug, Default, Copy, Clone)]
-pub enum Control<'a> {
-    #[default]
-    Unlimited,
-    Limited(&'a Counter, &'a Timer, &'a Trigger),
+/// Controls the search flow.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum ControlFlow {
+    /// Continue searching.
+    Continue,
+    /// Stop searching as soon as possible.
+    Stop,
+    /// Stop searching immediately.
+    Interrupt,
 }
 
-impl Control<'_> {
-    /// A reference to the node counter.
-    #[inline(always)]
-    pub fn counter(&self) -> &Counter {
-        static INFINITE: Counter = Counter::new(u64::MAX);
+/// The search control.
+#[derive(Debug)]
+pub struct Control {
+    limits: Limits,
+    interrupted: AtomicBool,
+    nodes: AtomicU64,
+    time: Range<Duration>,
+    timer: Instant,
+}
 
-        match self {
-            Control::Unlimited => &INFINITE,
-            Control::Limited(counter, _, _) => counter,
+impl Control {
+    #[inline(always)]
+    fn time_to_search(pos: &Position, limits: &Limits) -> Range<Duration> {
+        let Limits::Clock(clock, inc) = *limits else {
+            return limits.time()..limits.time();
+        };
+
+        let time_left = clock.saturating_sub(inc);
+        let moves_left = 256 / pos.fullmoves().get().min(64);
+        let time_per_move = inc.saturating_add(time_left / moves_left).min(clock / 2);
+        time_per_move / 2..time_per_move
+    }
+
+    /// Sets up the controller for a new search.
+    #[inline(always)]
+    pub fn new(pos: &Position, limits: Limits) -> Control {
+        Control {
+            interrupted: AtomicBool::new(false),
+            nodes: AtomicU64::new(limits.nodes()),
+            time: Self::time_to_search(pos, &limits),
+            timer: Instant::now(),
+            limits,
         }
     }
 
-    /// A reference to the timer.
+    /// The search limits.
     #[inline(always)]
-    pub fn timer(&self) -> &Timer {
-        static INFINITE: Timer = Timer::infinite();
-
-        match self {
-            Control::Unlimited => &INFINITE,
-            Control::Limited(_, timer, _) => timer,
-        }
+    pub fn limits(&self) -> &Limits {
+        &self.limits
     }
 
-    /// Whether the search should be interrupted.
+    /// The time elapsed so far.
     #[inline(always)]
-    pub fn interrupted(&self) -> Result<(), Interrupted> {
-        if let Control::Limited(nodes, timer, trigger) = self {
-            nodes.count().ok_or(Interrupted)?;
-            timer.remaining().ok_or(Interrupted)?;
-            if !trigger.is_armed() {
-                return Err(Interrupted);
-            }
+    pub fn time(&self) -> Duration {
+        Instant::now()
+            .saturating_duration_since(self.timer)
+            .max(Duration::from_nanos(1))
+    }
+
+    /// The nodes counted so far.
+    #[inline(always)]
+    pub fn nodes(&self) -> u64 {
+        self.limits.nodes() - self.nodes.load(Ordering::Relaxed)
+    }
+
+    /// Interrupts an ongoing search.
+    ///
+    /// Returns `true` if an ongoing search was interrupted.
+    pub fn interrupt(&self) -> bool {
+        !self.interrupted.fetch_or(true, Ordering::Relaxed)
+    }
+
+    /// Whether the search should expand a node.
+    #[inline(always)]
+    pub fn check(&self) -> ControlFlow {
+        use Ordering::Relaxed;
+        if self.interrupted.load(Relaxed) {
+            return ControlFlow::Interrupt;
         }
 
-        Ok(())
+        let checked_dec = |i: u64| i.checked_sub(1);
+        let nodes = match self.nodes.fetch_update(Relaxed, Relaxed, checked_dec) {
+            Ok(count) => self.limits.nodes() - count,
+            Err(_) => return ControlFlow::Interrupt,
+        };
+
+        if nodes % 256 > 0 {
+            return ControlFlow::Continue;
+        }
+
+        let time = self.time();
+        if time >= self.time.end {
+            ControlFlow::Interrupt
+        } else if time >= self.time.start {
+            ControlFlow::Stop
+        } else {
+            ControlFlow::Continue
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+    use test_strategy::proptest;
+
+    #[proptest]
+    fn measures_time_elapsed(pos: Position, l: Limits) {
+        let ctrl = Control::new(&pos, l);
+        let duration = Duration::from_millis(1);
+        thread::sleep(duration);
+        assert!(ctrl.time() >= duration);
+    }
+
+    #[proptest]
+    fn time_elapsed_is_always_positive(pos: Position, l: Limits) {
+        let ctrl = Control::new(&pos, l);
+        assert!(ctrl.time() > Duration::ZERO);
+    }
+
+    #[proptest]
+    fn interrupts_if_time_is_up(pos: Position) {
+        let ctrl = Control::new(&pos, Limits::Time(Duration::ZERO));
+        assert_eq!(ctrl.check(), ControlFlow::Interrupt);
+    }
+
+    #[proptest]
+    fn counts_nodes_searched(pos: Position, n: u64) {
+        let ctrl = Control::new(&pos, Limits::Nodes(n));
+        assert_eq!(ctrl.nodes(), 0);
+        assert_eq!(ctrl.check(), ControlFlow::Continue);
+        assert_eq!(ctrl.nodes(), 1);
+    }
+
+    #[proptest]
+    fn interrupts_if_node_count_is_reached(pos: Position) {
+        let ctrl = Control::new(&pos, Limits::Nodes(0));
+        assert_eq!(ctrl.check(), ControlFlow::Interrupt);
+    }
+
+    #[proptest]
+    fn interrupts_upon_request(pos: Position) {
+        let ctrl = Control::new(&pos, Limits::None);
+        assert!(ctrl.interrupt());
+        assert_eq!(ctrl.check(), ControlFlow::Interrupt);
+    }
+
+    #[proptest]
+    fn can_only_be_interrupted_once(pos: Position) {
+        let ctrl = Control::new(&pos, Limits::None);
+        assert!(ctrl.interrupt());
+        assert!(!ctrl.interrupt());
+        assert!(!ctrl.interrupt());
     }
 }

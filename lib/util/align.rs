f0148bd2fce35eda78f90a49d92879e3095eaee0
Bruno Dutra <brunocodutra@gmail.com>
2025-05-15 10:58:30 +0200

    introduce structs Moves and PackedMoves to wrap ArrayVec
    --------------------------------------------------
    Results of dev vs base (1+0.01, 1t, 16MB, UHO_Lichess_4852_v1.epd):
    Elo: 3.28 +/- 2.37, nElo: 5.55 +/- 4.02
    LOS: 99.66 %, DrawRatio: 47.31 %, PairsRatio: 1.06
    Games: 28740, Wins: 7554, Losses: 7283, Draws: 13903, Points: 14505.5 (50.47 %)
    Ptnml(0-2): [384, 3291, 6799, 3462, 434], WL/DD Ratio: 0.90
    LLR: 2.89 (100.0%) (-2.25, 2.89) [0.00, 3.00]
    --------------------------------------------------

--- lib/util/align.rs
+++ lib/util/align.rs
@@ -3,4 +3,4 @@ use derive_more::with_trait::{Deref, DerefMut, IntoIterator};
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash, Deref, DerefMut, IntoIterator)]
 #[cfg_attr(test, derive(test_strategy::Arbitrary))]
 #[repr(align(64))]
-pub struct AlignTo64<T>(#[cfg_attr(test, into_iterator(owned, ref, ref_mut))] pub T);
+pub struct AlignTo64<T>(#[into_iterator(owned, ref, ref_mut)] pub T);

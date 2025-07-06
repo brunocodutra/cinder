59c2d9d26a8e8835de254270a6b146787a0c36ea
Bruno Dutra <brunocodutra@gmail.com>
2025-07-06 11:17:45 +0200

    allow hyper-parameters to be negative

--- lib/params/constant.rs
+++ lib/params/constant.rs
@@ -1,16 +1,17 @@
-#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash)]
+use crate::params::Param;
+use derive_more::with_trait::Constructor;
+
+pub type Scalar<const V: i32> = Constant<V>;
+
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash, Constructor)]
 #[cfg_attr(test, derive(test_strategy::Arbitrary))]
-pub struct Param<const V: i32, const K: i32 = 1> {}
+pub struct Constant<const V: i32> {}
 
-impl<const V: i32, const K: i32> Param<V, K> {
-    #[inline(always)]
-    pub const fn new() -> Self {
-        const { assert!(V >= 0) }
-        Self {}
-    }
+impl<const V: i32> Param for Constant<V> {
+    type Value = i32;
 
     #[inline(always)]
-    pub const fn get(&self) -> i32 {
+    fn get(&self) -> Self::Value {
         V
     }
 }
--- lib/params/variable.rs
+++ lib/params/variable.rs
@@ -1,10 +1,69 @@
-use crate::params::Params;
+use crate::params::{Param, Params};
 use derive_more::with_trait::{Display, Error};
 use serde::{Deserialize, Serialize};
-use std::cmp::max;
 
 #[cfg(test)]
 use proptest::prelude::*;
+
+/// The reason why constructing [`Param`] from a floating point failed.
+#[derive(Debug, Display, Clone, Eq, PartialEq, Error)]
+#[display("value out of range")]
+pub struct ValueOutOfRange;
+
+#[derive(Debug, Default, Copy, Clone, PartialEq)]
+#[cfg_attr(test, derive(test_strategy::Arbitrary))]
+#[cfg_attr(feature = "spsa", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "spsa", serde(into = "f64"))]
+#[cfg_attr(feature = "spsa", serde(try_from = "f64"))]
+#[repr(transparent)]
+pub struct Constant<const V: i32> {}
+
+impl<const V: i32> Constant<V> {
+    pub const fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<const V: i32> Param for Constant<V> {
+    const LEN: usize = 0;
+    type Value = i32;
+
+    fn get(&self) -> Self::Value {
+        V
+    }
+
+    fn min() -> Self::Value {
+        V
+    }
+
+    fn max() -> Self::Value {
+        V
+    }
+
+    fn perturb<I: IntoIterator<Item = f64>>(&self, _: I) -> (Self, Self) {
+        (*self, *self)
+    }
+
+    fn update<I: IntoIterator<Item = f64>>(&mut self, _: I) {}
+}
+
+impl<const V: i32> From<Constant<V>> for f64 {
+    fn from(_: Constant<V>) -> Self {
+        V as _
+    }
+}
+
+impl<const V: i32> TryFrom<f64> for Constant<V> {
+    type Error = ValueOutOfRange;
+
+    fn try_from(value: f64) -> Result<Self, Self::Error> {
+        if !(Self::min()..=Self::max()).contains(&(value as i32)) {
+            return Err(ValueOutOfRange);
+        }
+
+        Ok(Constant {})
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 #[cfg_attr(test, derive(test_strategy::Arbitrary))]
@@ -12,37 +71,38 @@ use proptest::prelude::*;
 #[cfg_attr(feature = "spsa", serde(into = "f64"))]
 #[cfg_attr(feature = "spsa", serde(try_from = "f64"))]
 #[repr(transparent)]
-pub struct Param<const V: i32, const K: i32 = 1> {
+pub struct Scalar<const V: i32> {
     #[cfg_attr(test, strategy((Self::min()..=Self::max()).prop_map(|i| i as f64)))]
     value: f64,
 }
 
-impl<const V: i32, const K: i32> Param<V, K> {
-    const RANGE: i32 = if V < Params::BASE / 4 {
-        K * Params::BASE / 4
-    } else {
-        K * V
-    };
-
+impl<const V: i32> Scalar<V> {
     pub const fn new() -> Self {
-        const { assert!(V >= 0) }
-        const { assert!(K >= 0) }
         Self { value: V as f64 }
     }
 
-    pub fn min() -> i32 {
-        max(V - Self::RANGE / 2, 0)
+    pub fn range() -> i32 {
+        V.abs().max(Params::BASE / 4)
+    }
+}
+
+impl<const V: i32> Param for Scalar<V> {
+    const LEN: usize = 1;
+    type Value = i32;
+
+    fn get(&self) -> Self::Value {
+        self.value as _
     }
 
-    pub fn max() -> i32 {
-        Self::min() + Self::RANGE
+    fn min() -> Self::Value {
+        V - Self::range() / 2
     }
 
-    pub fn get(&self) -> i32 {
-        self.value as i32
+    fn max() -> Self::Value {
+        Self::min() + Self::range()
     }
 
-    pub fn perturb<I: IntoIterator<Item = f64>>(&self, perturbations: I) -> (Self, Self) {
+    fn perturb<I: IntoIterator<Item = f64>>(&self, perturbations: I) -> (Self, Self) {
         let mut perturbations = perturbations.into_iter();
         let (mut left, mut right) = (Self::default(), Self::default());
         let (min, max) = (Self::min() as f64, Self::max() as f64);
@@ -53,7 +113,7 @@ impl<const V: i32, const K: i32> Param<V, K> {
         (left, right)
     }
 
-    pub fn update<I: IntoIterator<Item = f64>>(&mut self, corrections: I) {
+    fn update<I: IntoIterator<Item = f64>>(&mut self, corrections: I) {
         let mut corrections = corrections.into_iter();
         let (min, max) = (Self::min() as f64, Self::max() as f64);
         let delta = (max - min) * corrections.next().unwrap();
@@ -61,33 +121,28 @@ impl<const V: i32, const K: i32> Param<V, K> {
     }
 }
 
-impl<const V: i32, const K: i32> Default for Param<V, K> {
+impl<const V: i32> Default for Scalar<V> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<const V: i32, const K: i32> From<Param<V, K>> for f64 {
-    fn from(param: Param<V, K>) -> Self {
-        param.value
+impl<const V: i32> From<Scalar<V>> for f64 {
+    fn from(scalar: Scalar<V>) -> Self {
+        scalar.value
     }
 }
 
-/// The reason why constructing [`Param`] from a floating point failed.
-#[derive(Debug, Display, Clone, Eq, PartialEq, Error)]
-#[display("parameter out of range")]
-pub struct ParameterOutOfRange;
-
-impl<const V: i32, const K: i32> TryFrom<f64> for Param<V, K> {
-    type Error = ParameterOutOfRange;
+impl<const V: i32> TryFrom<f64> for Scalar<V> {
+    type Error = ValueOutOfRange;
 
     fn try_from(value: f64) -> Result<Self, Self::Error> {
-        let param = Param { value };
-        if !(Self::min()..=Self::max()).contains(&param.get()) {
-            return Err(ParameterOutOfRange);
+        let scalar = Scalar { value };
+        if !(Self::min()..=Self::max()).contains(&scalar.get()) {
+            return Err(ValueOutOfRange);
         }
 
-        Ok(param)
+        Ok(scalar)
     }
 }
 
@@ -96,29 +151,61 @@ mod tests {
     use super::*;
     use test_strategy::proptest;
 
-    type MockParam = Param<1000>;
+    type MockConstant = Constant<400>;
+    type MockScalar = Scalar<400>;
 
     #[proptest]
-    fn param_value_is_always_within_bounds(p: MockParam) {
-        assert!((MockParam::min()..=MockParam::max()).contains(&p.get()));
+    fn constant_value_is_always_within_bounds(p: MockConstant) {
+        assert!((MockConstant::min()..=MockConstant::max()).contains(&p.get()));
     }
 
     #[proptest]
-    fn param_has_an_equivalent_array_of_values(p: MockParam) {
-        assert_eq!(Param::try_from(<f64>::from(p)), Ok(p));
+    fn constant_has_an_equivalent_value(p: MockConstant) {
+        assert_eq!(Constant::try_from(<f64>::from(p)), Ok(p));
     }
 
     #[proptest]
-    fn converting_param_from_array_succeeds_if_all_values_in_range(
-        #[strategy(MockParam::min() as f64..=MockParam::max() as f64)] vs: f64,
+    fn converting_constant_from_fails_if_value_too_large(
+        #[strategy(MockConstant::max() as f64 + 1.0..)] v: f64,
     ) {
-        assert_ne!(MockParam::try_from(vs), Err(ParameterOutOfRange));
+        assert_eq!(MockConstant::try_from(v), Err(ValueOutOfRange));
     }
 
     #[proptest]
-    fn converting_param_from_array_fails_if_any_value_out_of_range(
-        #[strategy(MockParam::max() as f64 + 1.0..)] vs: f64,
+    fn converting_constant_from_fails_if_value_too_small(
+        #[strategy(..=MockConstant::min() as f64 - 1.0)] v: f64,
     ) {
-        assert_eq!(MockParam::try_from(vs), Err(ParameterOutOfRange));
+        assert_eq!(MockConstant::try_from(v), Err(ValueOutOfRange));
+    }
+
+    #[proptest]
+    fn scalar_value_is_always_within_bounds(p: MockScalar) {
+        assert!((MockScalar::min()..=MockScalar::max()).contains(&p.get()));
+    }
+
+    #[proptest]
+    fn scalar_has_an_equivalent_value(p: MockScalar) {
+        assert_eq!(Scalar::try_from(<f64>::from(p)), Ok(p));
+    }
+
+    #[proptest]
+    fn converting_scalar_succeeds_value_in_range(
+        #[strategy(MockScalar::min() as f64..=MockScalar::max() as f64)] v: f64,
+    ) {
+        assert_ne!(MockScalar::try_from(v), Err(ValueOutOfRange));
+    }
+
+    #[proptest]
+    fn converting_scalar_from_fails_if_value_too_large(
+        #[strategy(MockScalar::max() as f64 + 1.0..)] v: f64,
+    ) {
+        assert_eq!(MockScalar::try_from(v), Err(ValueOutOfRange));
+    }
+
+    #[proptest]
+    fn converting_scalar_from_fails_if_value_too_small(
+        #[strategy(..=MockScalar::min() as f64 - 1.0)] v: f64,
+    ) {
+        assert_eq!(MockScalar::try_from(v), Err(ValueOutOfRange));
     }
 }

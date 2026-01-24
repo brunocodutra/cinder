3bf422d46acebc60e74aecd47a5969767d812098
Bruno Dutra <brunocodutra@gmail.com>
2026-01-24 17:42:46 +0100

    reduce unrolling during nnue inference to avoid register spillover
    VNNI512
    
    --------------------------------------------------
    Results of dev vs base (1+0.01, 1t, 16MB, UHO_Lichess_4852_v1.epd):
    Elo: 0.31 +/- 1.55, nElo: 0.58 +/- 2.89
    LOS: 65.40 %, DrawRatio: 52.52 %, PairsRatio: 1.01
    Games: 55448, Wins: 14419, Losses: 14369, Draws: 26660, Points: 27749.0 (50.05 %)
    Ptnml(0-2): [470, 6079, 14562, 6157, 456], WL/DD Ratio: 1.02
    LLR: 2.91 (100.7%) (-2.25, 2.89) [-3.00, 1.00]
    --------------------------------------------------
    
    AVX512
    
    --------------------------------------------------
    Results of dev vs base (1+0.01, 1t, 16MB, UHO_Lichess_4852_v1.epd):
    Elo: 40.46 +/- 9.31, nElo: 71.74 +/- 16.35
    LOS: 100.00 %, DrawRatio: 45.79 %, PairsRatio: 2.18
    Games: 1734, Wins: 579, Losses: 378, Draws: 777, Points: 967.5 (55.80 %)
    Ptnml(0-2): [7, 141, 397, 288, 34], WL/DD Ratio: 1.28
    LLR: 2.90 (100.3%) (-2.25, 2.89) [0.00, 3.00]
    --------------------------------------------------

--- lib/nnue/input.rs
+++ lib/nnue/input.rs
@@ -2,7 +2,7 @@ use crate::nnue::{FTQ, HLS, L1, Layer, Ln, Synapse, Value};
 use crate::simd::*;
 use crate::util::{Assume, Float};
 use bytemuck::Zeroable;
-use std::{array::from_fn as each, ops::Mul};
+use std::{array, ops::Mul};
 
 const I: usize = L1::LEN;
 const O: usize = Ln::LEN / 2;
@@ -49,12 +49,24 @@ impl<S: for<'a> Synapse<Input<'a> = Ln<'a>, Output = V2<f32>>> Synapse for Input
         let us: &[[V4<i16>; I / W8]; 2] = us.cast();
         let them: &[[V4<i16>; I / W8]; 2] = them.cast();
 
-        let active: Aligned<[V8<u8>; I / W8]> = Aligned(each(|i| {
-            let xl0 = us[0][i].simd_min(Simd::splat(FTQ));
-            let xl1 = them[0][i].simd_min(Simd::splat(FTQ));
-            let xh0 = us[1][i].simd_clamp(Simd::splat(0), Simd::splat(FTQ));
-            let xh1 = them[1][i].simd_clamp(Simd::splat(0), Simd::splat(FTQ));
-            xl0.mul_high::<9>(xh0).pack(xl1.mul_high::<9>(xh1))
+        let active: Aligned<[[V8<u8>; 2]; I / W8 / 2]> = Aligned(array::from_fn(|i| {
+            let xl00 = us[0][2 * i].simd_min(Simd::splat(FTQ));
+            let xl01 = us[0][2 * i + 1].simd_min(Simd::splat(FTQ));
+            let xh00 = us[1][2 * i].simd_clamp(Simd::splat(0), Simd::splat(FTQ));
+            let xh01 = us[1][2 * i + 1].simd_clamp(Simd::splat(0), Simd::splat(FTQ));
+
+            let x00 = xl00.mul_high::<9>(xh00);
+            let x01 = xl01.mul_high::<9>(xh01);
+
+            let xl10 = them[0][2 * i].simd_min(Simd::splat(FTQ));
+            let xl11 = them[0][2 * i + 1].simd_min(Simd::splat(FTQ));
+            let xh10 = them[1][2 * i].simd_clamp(Simd::splat(0), Simd::splat(FTQ));
+            let xh11 = them[1][2 * i + 1].simd_clamp(Simd::splat(0), Simd::splat(FTQ));
+
+            let x10 = xl10.mul_high::<9>(xh10);
+            let x11 = xl11.mul_high::<9>(xh11);
+
+            [Pack::pack(x00, x10), Pack::pack(x01, x11)]
         }));
 
         let mut nzs_len = 0;
@@ -75,41 +87,57 @@ impl<S: for<'a> Synapse<Input<'a> = Ln<'a>, Output = V2<f32>>> Synapse for Input
             }
         }
 
-        const K: usize = usize::max(8 * W2 / O, 1);
-        let mut acc = Aligned([[Simd::splat(0); K]; O / W2]);
+        #[cfg(any(target_feature = "avx2", target_feature = "neon"))]
+        const K: usize = 4;
+
+        #[cfg(not(any(target_feature = "avx2", target_feature = "neon")))]
+        const K: usize = 2;
+
         let xs: &[u8x4; I / 4] = active.cast();
         let ws: &[[V8<i8>; O / W2]; I / 4] = self.weight.cast();
+        let bs: &[V2<f32>; O / W2] = self.bias.cast();
+
+        let mut acc = Aligned([[Simd::splat(0); K]; O / W2]);
         let mut nzs = nzs[..nzs_len].iter().copied().array_chunks::<K>();
 
         for nzs in &mut nzs {
+            let x0 = *Aligned([*xs.get(nzs[0] as usize).assume(); W2]).cast();
+            let x1 = *Aligned([*xs.get(nzs[1] as usize).assume(); W2]).cast();
+
+            #[cfg(any(target_feature = "avx2", target_feature = "neon"))]
+            let x2 = *Aligned([*xs.get(nzs[2] as usize).assume(); W2]).cast();
+            #[cfg(any(target_feature = "avx2", target_feature = "neon"))]
+            let x3 = *Aligned([*xs.get(nzs[3] as usize).assume(); W2]).cast();
+
             for (j, a) in acc.iter_mut().enumerate() {
-                *a = each(|k| {
-                    let x = xs.get(nzs[k] as usize).assume();
-                    let w = ws.get(nzs[k] as usize).assume()[j];
-                    w.mul_add_4x8(*Aligned([*x; W2]).cast(), a[k])
-                });
+                {
+                    a[0] = ws.get(nzs[0] as usize).assume()[j].mul_add_4x8(x0, a[0]);
+                    a[1] = ws.get(nzs[1] as usize).assume()[j].mul_add_4x8(x1, a[1]);
+                }
+
+                #[cfg(any(target_feature = "avx2", target_feature = "neon"))]
+                {
+                    a[2] = ws.get(nzs[2] as usize).assume()[j].mul_add_4x8(x2, a[2]);
+                    a[3] = ws.get(nzs[3] as usize).assume()[j].mul_add_4x8(x3, a[3]);
+                }
             }
         }
 
         for nz in nzs.into_remainder() {
-            let x = xs.get(nz as usize).assume();
+            let x = *Aligned([*xs.get(nz as usize).assume(); W2]).cast();
             for (j, a) in acc.iter_mut().enumerate() {
-                let w = ws.get(nz as usize).assume()[j];
-                a[0] = w.mul_add_4x8(*Aligned([*x; W2]).cast(), a[0]);
+                a[0] = ws.get(nz as usize).assume()[j].mul_add_4x8(x, a[0]);
             }
         }
 
-        let mut output = self.bias.clone();
-        let os: &mut [V2<f32>; O / W2] = output.cast_mut();
-
-        *os = each(|j| {
+        let output: Aligned<[V2<f32>; O / W2]> = Aligned(array::from_fn(|j| {
             let sum = acc[j].iter().sum::<V2<i32>>();
-            sum.cast::<f32>().mul_add(Simd::splat(I2F), os[j])
-        });
+            sum.cast::<f32>().mul_add(Simd::splat(I2F), bs[j])
+        }));
 
         let active = Aligned([
-            os.map(|i| i.simd_max(Simd::splat(0.)).powi::<2>()),
-            os.map(|i| i.simd_min(Simd::splat(0.)).powi::<2>()),
+            output.map(|i| i.simd_max(Simd::splat(0.)).powi::<2>()),
+            output.map(|i| i.simd_min(Simd::splat(0.)).powi::<2>()),
         ]);
 
         let result = self.next.forward(active.cast()).reduce_sum();

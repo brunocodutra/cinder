46a6be26b7ec0c072b514fd4d9892bb5419f3fa1
Bruno Dutra <brunocodutra@gmail.com>
2026-02-18 22:29:46 +0100

    research null-window at full depth if reduced depth null-window search fails high
    --------------------------------------------------
    Results of dev vs base (4+0.04, 1t, 16MB, UHO_Lichess_4852_v1.epd):
    Elo: 1.86 +/- 1.79, nElo: 3.56 +/- 3.42
    LOS: 97.94 %, DrawRatio: 49.64 %, PairsRatio: 1.04
    Games: 39608, Wins: 10072, Losses: 9860, Draws: 19676, Points: 19910.0 (50.27 %)
    Ptnml(0-2): [136, 4744, 9830, 4960, 134], WL/DD Ratio: 0.97
    LLR: 5.99 (207.2%) (-2.25, 2.89) [-3.00, 1.00]
    --------------------------------------------------

--- lib/params.rs
+++ lib/params.rs
@@ -286,6 +286,8 @@ params! {
     lmr_gives_check: [-0.46384782, 0.015132483, 0.011271138],
     lmr_history: [0.0012702212, -0.003735534],
     lmr_counter: [0.008251999],
+    lmr_limit: [0.0],
+    lmr_threshold: [0.5],
     killer_rating: [53.16652],
     history_rating: [108.39066, 124.212105, 124.212105],
     good_noisy_margin: [-20.445045],
